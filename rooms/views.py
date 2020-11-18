26eb83bfa200e802b68dccadf4ae0184713891a3
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2020-11-18 21:29:17 +0900

    View Feature: class View

--- rooms/views.py
+++ rooms/views.py
@@ -1,25 +1,38 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 
 
+class HomeView(ListView):
+
+    """ Home View Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    allow_empty = False
+    context_object_name = "rooms"
+
+
+"""   function base View
 def all_rooms(request):
     page = request.GET.get("page", 1)
     page = int(page or 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 2)
-    rooms = paginator.get_page(page)
+    paginator = Paginator(room_list, 2, orphans=1)
+    try:
+        rooms = paginator.page(page)
+    except EmptyPage:
+        return redirect("/")
 
     page_numbers_range = 10
 
     max_index = rooms.paginator.num_pages
     start_index = int((page - 1) / page_numbers_range) * page_numbers_range
-    print(start_index)
     end_index = start_index + page_numbers_range
-
     if end_index >= max_index:
         end_index = max_index
+
     paginator_range = paginator.page_range[start_index:end_index]
 
     return render(
@@ -27,3 +40,4 @@ def all_rooms(request):
         "rooms/home.html",
         {"rooms": rooms, "paginator_range": paginator_range},
     )
+"""
