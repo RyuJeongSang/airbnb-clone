50345718fcd7b6f8e2ba24e8414c01eef2f9bcd3
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2020-11-18 19:45:55 +0900

    View Feature: home page and pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,29 @@
+from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from . import models
 
-# Create your views here.
+
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 2)
+    rooms = paginator.get_page(page)
+
+    page_numbers_range = 10
+
+    max_index = rooms.paginator.num_pages
+    start_index = int((page - 1) / page_numbers_range) * page_numbers_range
+    print(start_index)
+    end_index = start_index + page_numbers_range
+
+    if end_index >= max_index:
+        end_index = max_index
+    paginator_range = paginator.page_range[start_index:end_index]
+
+    return render(
+        request,
+        "rooms/home.html",
+        {"rooms": rooms, "paginator_range": paginator_range},
+    )
