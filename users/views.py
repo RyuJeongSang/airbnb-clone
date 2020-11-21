8e4c172deea771a607217ddbbef8ee951c17a12c
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2020-11-21 17:15:16 +0900

    View Feature: Login and Logout make easier with FormView and LogoutView

--- users/views.py
+++ users/views.py
@@ -1,29 +1,31 @@
-from django.views import View
+from django.views.generic import FormView
+from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
+class LoginView(FormView):
 
     """ LoginView Definition """
 
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+class LogoutView(LogoutView):
+
+    """ LogoutView Definition """
+
     def get(self, request):
-        form = forms.LoginForm()
-        return render(request, "users/login.html", {"form": form})
-
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
-
-
-def log_out(request):
-    logout(request)
-    return redirect(reverse("core:home"))
+        return redirect(reverse("core:home"))
