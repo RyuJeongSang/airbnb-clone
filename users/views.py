d526b6e3233a8df6f5a207ce47fed7191b0d53c0
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2020-11-22 16:50:49 +0900

    View Feature: sign up page

--- users/views.py
+++ users/views.py
@@ -29,3 +29,21 @@ class LogoutView(LogoutView):
 
     def get(self, request):
         return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    """ SignUpView Definition """
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
